c9f7c0cbbd548937ae5ff9ee238d62132abfb498
Nicholas Wise <nicholasmwise@gmail.com>
2025-05-16 16:15:16 -0500

    Added in starting work on the UI for the main Menu

---
+++ src/errour_ui.rs
@@ -0,0 +1,19 @@
+use macroquad::prelude::*;
+
+use macroquad::ui::{hash, root_ui, widgets, Skin};
+
+pub fn draw_ui() {
+    root_ui().window(hash!(), vec2(20., 250.), vec2(300., 300.), |ui| {
+            widgets::Button::new("Play")
+                .position(vec2(65.0, 15.0))
+                .ui(ui);
+            widgets::Button::new("Options")
+                .position(vec2(40.0, 75.0))
+                .ui(ui);
+
+            widgets::Button::new("Quit")
+                .position(vec2(65.0, 195.0))
+                .ui(ui);
+        });
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use macroquad::prelude::*;
 use crate::utils::window_conf;
+use crate::errour_ui::draw_ui;
+
+use macroquad::ui::{hash, root_ui, widgets, Skin};
 
 mod utils;
+mod errour_ui;
 
-#[macroquad::main(window_conf)]
+#[macroquad::main("main_menu")]
 async fn main() {
+    window_conf();
+    let window_style = root_ui()
+        .style_builder()
+        .background_margin(RectOffset::new(32.0, 76.0, 44.0, 20.0))
+        .margin(RectOffset::new(0.0, -40.0, 0.0, 0.0))
+        .build();
+
     utils::scale_screen();
     loop {
         clear_background(RED);
+
+        draw_ui(); 
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
@@ -19,3 +32,4 @@ async fn main() {
         next_frame().await
     }
 }
+
