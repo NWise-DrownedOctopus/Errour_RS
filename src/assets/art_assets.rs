e0598b090824b7830e2bd1ac8169e3acb9224932
NWise-DrownedOctopus <nicholasmwise@gmail.com>
2025-09-02 13:53:26 -0500

    Moved assets to static file for web build access

--- src/assets/art_assets.rs
+++ src/assets/art_assets.rs
@@ -17,10 +17,10 @@ impl GameArtAssets {
     }
 
     pub async fn load() -> Self {
-        let enemy = load_texture("art/vindex/enemies_01.png").await.unwrap();
-        let player_base = load_texture("art/errour/player_base_01.png").await.unwrap();  
-        let ground = load_texture("art/environment/spots.png").await.unwrap();
-        let projectile = load_texture("art/vfx/projectiles_01.png").await.unwrap();
+        let enemy = load_texture("static/art/vindex/enemies_01.png").await.unwrap();
+        let player_base = load_texture("static/art/errour/player_base_01.png").await.unwrap();  
+        let ground = load_texture("static/art/environment/spots.png").await.unwrap();
+        let projectile = load_texture("static/art/vfx/projectiles_01.png").await.unwrap();
         
         enemy.set_filter(FilterMode::Nearest);
         player_base.set_filter(FilterMode::Nearest); 
